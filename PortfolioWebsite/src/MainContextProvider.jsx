9432603d990f3a443dd740ed58520e98c2ba6b86
Alex-FonsecaN <fonsecaalexandre30@gmail.com>
2024-12-07 12:46:23 -0300

    Atualizacao de texto

--- PortfolioWebsite/src/MainContextProvider.jsx
+++ PortfolioWebsite/src/MainContextProvider.jsx
@@ -18,7 +18,7 @@ export const MainContextProvider = ({ children }) => {
         "Sou um profissional organizado, dedicado e respeitoso. Abraço as mudanças sempre que necessárias, e os desafios me mantêm motivado. Tenho habilidades em várias áreas da programação, com ênfase em desenvolvimento web, tanto no BackEnd quanto no FrontEnd.",
         "Estudo desenvolvimento web desde 2016, período em que adquiri grande familiaridade com HTML e CSS. Com o passar dos anos, expandi meu conhecimento para outras tecnologias que hoje enriquecem meus projetos, incluindo ReactJs, NestJs, NodeJs, SASS e Tailwind.",
         "Além de desenvolver para a web, também sou um desenvolvedor de jogos! Participei de projetos publicados, como 'Dandara: Trials of Fear' e 'Nonogram Logic Pic: Pictogram', além de diversos outros, incluindo projetos acadêmicos e pequenos jogos em WebGL sob demanda.",
-        "Gosto de explorar outras áreas, uma delas sendo a criação de filtros para TikTok. Já criei e publiquei diversos filtros, praticando minha criatividade e cultivando isso como um hobby pessoal."
+        "Gosto de explorar outras áreas, uma delas sendo a criação de automatizações em Python e integrações com Inteligência Artificial. Já criei projetos muito interessantes e úteis profissionalmente."
     ];
 
     const publics = {
